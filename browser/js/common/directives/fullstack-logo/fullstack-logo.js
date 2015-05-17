ad5e555d917fa8b2c89383ac16c7fedc5c22cedc
Sean Kim <nogever@gmail.com>
2015-05-16 20:40:20 -0400

    users can change background image

--- browser/js/common/directives/fullstack-logo/fullstack-logo.js
+++ browser/js/common/directives/fullstack-logo/fullstack-logo.js
@@ -1,7 +1,20 @@
 'use strict';
-app.directive('fullstackLogo', function () {
+
+app.directive('fullstackLogo', function() {
+
+	var controller = function($scope, $element) {
+
+		$scope.offCanvas = function() {
+			angular.element('html').toggleClass('off-canvas-on');
+			angular.element('.off-canvas').toggleClass('off-canvas-in')
+		}
+
+	};
+
     return {
         restrict: 'E',
-        templateUrl: 'js/common/directives/fullstack-logo/fullstack-logo.html'
+        templateUrl: 'js/common/directives/fullstack-logo/fullstack-logo.html',
+        controller: controller
     };
+    
 });
